3eb17cab9bb6c1baf35549ef14a5de736a598062
Nguyên Huy Hoàng <145022510+hoanggxyuuki@users.noreply.github.com>
2024-01-09 15:42:48 +0700

    finish
    thuyet trinh

--- main.py
+++ main.py
@@ -1,21 +1,17 @@
 from flask import Flask, render_template, request, jsonify
-import smtplib
 import re
 import requests
 import json
-
+dishes=requests.get("http://localhost:3000/doan").json()
+#khởi tạo Flask và trỏ đến trang chủ website
 app = Flask(__name__)
+@app.route("/")
+def home():
+    return render_template("home.html", dishes=dishes)
+#Token và id của bot telegram
 TELEGRAM_BOT_TOKEN = '6532818104:AAFkAxwTMthHrKHfjksCaGnKBSDuNIIexLs'  
 TELEGRAM_CHAT_ID = '6296558403'  
-
-def send_telegram_message(name, phone, address, mon_an):
-    if not name or not phone or not address:
-        return {'error': 'Vui lòng điền đầy đủ thông tin.'}
-    else:
-        url = f'https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage'
-        params = {'chat_id': TELEGRAM_CHAT_ID, 'text': f'Đơn đặt hàng mới:{mon_an}\nHọ tên: {name}\nSĐT: {phone}\nĐịa chỉ: {address}'}
-        response = requests.post(url, params = params)
-        return response.json()
+#gửi thông tin khách hàng về telegram
 @app.route('/send-telegram', methods=['POST'])
 def send_telegram_route():
     data = request.get_json()
@@ -23,36 +19,22 @@ def send_telegram_route():
     phone = data.get('phone')
     address = data.get('address')
     mon_an=data.get('mon_an')
-    response = send_telegram_message(name, phone, address,mon_an)
-
-    if response.get('ok'):
-        return jsonify({'success': True})
-    else:
-        return jsonify({'error': 'Hãy điền đầy đủ thông tin.'})
-
-with open('doan.json','r', encoding='utf-8') as file:
-    data = json.load(file)
-    dishes=data.get('doan',[])
-
-@app.route("/")
-def home():
-    return render_template("home.html", dishes=dishes)
-
-
-@app.route('/validate', methods=['POST'])    
-def validate():
-    data = request.get_json()
-    name = data.get('name')
-    phone = data.get('phone')
-    address = data.get('address')
+    so_luong=data.get('so_luong')
+    #kiểm tra thông tin khách hàng nhập vào
     if not validate_name(name):
         return jsonify({'error': 'Vui lòng nhập họ tên có dấu và không có số.'})
     if not validate_phone_number(phone):
         return jsonify({'error': 'Vui lòng nhập số điện thoại hợp lệ.'})
     if not validate_address(address):
         return jsonify({'error': 'Vui lòng nhập địa chỉ không để trống.'})
+    else:
+        
+        response = send_telegram_message(name, phone, address,mon_an,so_luong)
 
-    return jsonify({'successMessage': 'Thông tin đã được xác nhận thành công!'})
+        if response.get('ok'):
+            return jsonify({'success': True})
+        else:
+            return jsonify({'error': False})
 
 def validate_name(name):
     return bool(re.match(r'^[a-zA-Z\sáàảãạăắằẳẵặâấầẩẫậéèẻẽẹêếềểễệíìỉĩịóòỏõọôốồổỗộơớờởỡợúùủũụưứừửữựýỳỷỹỵĐđ]+$', name))
@@ -60,7 +42,14 @@ def validate_phone_number(phone):
     return bool(re.match(r'^\d{10}$', phone))
 def validate_address(address):
     return bool(address.strip())    
-
+def send_telegram_message(name, phone, address, mon_an,so_luong):
+    if not name or not phone or not address:
+        return {'error': 'Vui lòng điền đầy đủ thông tin.'}
+    else:
+        url = f'https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage'
+        params = {'chat_id': TELEGRAM_CHAT_ID, 'text': f'Đơn đặt hàng mới:{mon_an}\nSố lượng: {so_luong}\nHọ tên: {name}\nSĐT: {phone}\nĐịa chỉ: {address}'}
+        response = requests.post(url, params = params)
+        return response.json()
 
 if __name__ == "__main__":
    app.run(debug=True)
--- static/scripts/script.js
+++ static/scripts/script.js
@@ -1,7 +1,6 @@
+/*Lấy dữ liệu từ api được build bằng json-server và lưu vào biến dish */
 let dish = [];
-
 document.addEventListener('DOMContentLoaded', function () {
- 
   fetch('http://localhost:3000/doan')
     .then(response => response.json())
     .then(data => {
@@ -11,36 +10,32 @@ document.addEventListener('DOMContentLoaded', function () {
       console.error('Error fetching dishes:', error);
     });
   document.addEventListener('click', function (event) {
-    
+    /* ẩn popup khi người dùng chưa thao tác click vào hình ảnh*/
     if (!event.target.closest('.popup-recipe') && document.getElementById('popup-recipe').style.display === 'none') {
       hidePopup();
     }
   });
 });
-
+/*Hiển thị tên món ăn và hình ảnh đi kèm , thêm điều kiện click vào hình ảnh sẽ hiển thị popup*/
 function displayDishes(dishes) {
   const dishesContainer = document.getElementById('dishesContainer');
 
   dishes.forEach(dish => {
     const dishElement = document.createElement('div');
     dishElement.className = 'dish';
-
     dishElement.innerHTML = `
       <h2>${dish.name}</h2>
       <div class="thumb">
         <img src="${dish.img}" alt="Dish Image" onclick="showPopup(${dish})">
-      
         </div>
     `;
     dishElement.addEventListener('click', function () {
       showPopup(dish);
-
     });
     dishesContainer.appendChild(dishElement);
   });
 }
-
-
+/*Hiển thị thông tin món ăn khi click vào popup*/
 function showPopup(dish) {
   document.getElementById('ingredients').textContent = 'Nguyên liệu: ' + dish.ingredients;
   document.getElementById('instructions').textContent = 'Cách làm: ' + dish.instructions;
@@ -53,36 +48,21 @@ function hidePopup() {
   const popup = document.getElementById('popup-recipe');
   popup.style.display = 'none';
 }
-
 function showDetails(event) {
-  
   var details = event.target.parentNode.querySelector('.details');
-  
   details.style.display = "block";
 }
-
+/*Lấy tất cả phần tử có tên 'dish' , lọc lại từ phần tử món ăn và thêm vào class fadeIn*/
 window.addEventListener('scroll', function () {
   var dishes = document.querySelectorAll('.dish');
   dishes.forEach(dish => {
-    if (isInViewport(dish)) {
-      dish.classList.add('fadeIn');
-    }
+    
+    dish.classList.add('fadeIn');
+    
   });
 });
 
-
-let buttons = document.querySelectorAll('.btn-recipe');
-buttons.forEach(button => {
-  button.addEventListener('click', function () {
-    let recipe = this.parentElement.querySelector('.recipe');
-    recipe.style.display = 'block';
-  });
-});
-function isInViewport(element) {
-  // Lógic kiểm tra xem phần tử đã xuất hiện trong viewport hay chưa
-  return true / false;
-}
-
+/*Thêm mỗi sự kiện click cho từng hình ảnh*/
 let images = document.querySelectorAll('.dish img');
 let popup = document.querySelector('.popup-recipe');
 images.forEach(img => {
@@ -107,52 +87,51 @@ document.getElementById("quantity").addEventListener("click", function (event) {
   event.stopPropagation();
 });
 
+/*Lấy tên món ăn */
 function setDish(dish) {
   document.getElementById("mon_an").value = dish['name'];
   validateAndSendTelegram();
 }
+/* Lấy thông tin khách hàng và hiển thị kết quả trả về*/
 function validateAndAddToCart() {
   var nameInput = document.getElementById("name").value;
   var phoneInput = document.getElementById("phone").value;
   var addressInput = document.getElementById("address").value;
   var mon_anI = document.getElementById("mon_an").value;
+  var so_luongI=document.getElementById("quantity").value;
 
   // Gửi yêu cầu kiểm tra đến server
   var xhr = new XMLHttpRequest();
   xhr.open("POST", "/send-telegram", true);
   xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-
   xhr.onreadystatechange = function () {
     if (xhr.readyState == 4) {
-      if (xhr.status == 200) {
-        var response = JSON.parse(xhr.responseText);
-        alert("Đặt hàng thành công");
-      } else {
-        alert("Có lỗi kết nối đến server.");
-      }
+        if (xhr.status == 200) {
+            var response = JSON.parse(xhr.responseText);
+            
+            if (response.success) {
+                alert("Đặt hàng thành công");
+            } else if (response.error) {
+                alert("Có lỗi: " + response.error);
+            } else {
+                alert("Gửi tin nhắn thất bại.");
+            }
+        } else {
+            alert("Có lỗi kết nối đến server.");
+        }
     }
-  };
+};
+        
 
   var data = {
     name: nameInput,
     phone: phoneInput,
     address: addressInput,
-    mon_an: mon_anI
+    mon_an: mon_anI,
+    so_luong:so_luongI
   };
-
+  
   xhr.send(JSON.stringify(data));
+  
 }
 
-function handleValidationResponse(response) {
-  var messageBox = document.getElementById("message-box");
-  if (response.error) {
-    // Tùy chỉnh thông báo lỗi
-
-    alert("Lỗi: " + response.error);
-  } else {
- 
-
-    alert(response.successMessage);
-  }
-  messageBox.style.display = "block";
-}
